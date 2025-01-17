4d912a39d34033e88277082e34a39139f0a351d2
PaulCabreraEPN <paul.cabrera@epn.edu.ec>
2025-01-17 16:01:53 -0500

    login consumo endpoint

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,73 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Mensaje from '../context/alerts/Mensaje';
 
 const Login = () => {
+    //Declarciones
+    const frontendUrl = import.meta.env.VITE_URL_FRONTEND;
+    const backendUrl = import.meta.env.VITE_URL_BACKEND;
+    const [mensaje, setMensaje] = useState({})
+    const navigate = useNavigate()
+
+    //Crear un use state
+    const [form, setform] = useState({
+        username:"",
+        password:""
+    });
+    
+    //Setear valores 
+    const handeChange = (e) => {
+        setform({
+            ...form,
+            [e.target.name]:e.target.value
+        });
+    };
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const url = `${backendUrl}/login-admin`
+            const respuesta = await axios.post(url,form)
+            console.log(respuesta);
+            navigate('/dashboard')
+        } catch (error) {
+            setMensaje({respuesta:error.response.data.msg,tipo:false})
+            setTimeout(() => {
+                setMensaje({})
+            },5000);
+            setform({})
+        }
+    }
+
     return (
         <div className="flex h-screen">
             {/* Primera mitad: Formulario */}
             <div className="w-1/2 bg-white flex justify-center items-center padd">
-                <div className="md:w-4/5 sm:w-full">
-                    <h1 className="text-3xl font-semibold mb-2 text-center uppercase text-gray-500">
-                        Welcome back
-                    </h1>
-                    <small className="text-gray-400 block my-4 text-sm">
-                        Welcome back! Please enter your details
-                    </small>
+                <div className="md:w-3/5 sm:w-full">
+                    <div className='flex'>
+                    <div className="hidden sm:block bg-[url('/images/mainlogo.png')] bg-no-repeat bg-center bg-contain h-48 w-48 sm:h-14 sm:w-12">
+  {/* Aquí puedes agregar otros elementos si los necesitas */}
+</div>
+                        <h1 className="text-5xl font-semibold mb-2 text-center uppercase text-gray-500">
+                        PRIMA S.A.
+                        </h1>
+                        
+                    </div>
+                    <small className="text-gray-400 block mb-5 text-sm">Bienvenido a tu plataforma</small>
 
-                    <form>
+                    {Object.keys(mensaje).length>0 && <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>}
+
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-3">
                             <label className="mb-2 block text-lg font-semibold">Nombre de Usuario</label>
                             <input
-                                type="email"
-                                placeholder="Enter your email"
+                                type="text"
+                                placeholder="Ingresa tu nombre de usuario"
                                 className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500"
+                                name='username'
+                                onChange={handeChange}
+                                value={form.name}
                             />
                         </div>
 
@@ -29,6 +77,9 @@ const Login = () => {
                                 type="password"
                                 placeholder="********************"
                                 className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500"
+                                name='password'
+                                onChange={handeChange}
+                                value={form.password}
                             />
                         </div>
 
@@ -38,18 +89,10 @@ const Login = () => {
                         </div>
 
                         <div className="my-4 border-b-2 py-4">
-                            <Link
-                                to="/dashboard"
-                                className="py-2 w-full block text-center bg-gray-500 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-gray-900 hover:text-white"
-                            >
-                                Iniciar Sesión
-                            </Link>
+                        <button className="py-2 w-full block text-center bg-gray-500 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-gray-900 hover:text-white">Iniciar Sesión</button>
                         </div>
                     </form>
-
-                
                     
-
                     <div className="mt-5 text-xs ">
                         <Link
                             to="/forgot/id"
