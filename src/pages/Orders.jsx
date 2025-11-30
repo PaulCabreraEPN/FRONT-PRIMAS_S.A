d8bfa8bbc86bf4792852a15aee3fc87d6fc0e33e
OMEN <davidcatudiaz22@gmail.com>
2025-11-29 19:02:37 -0500

    Style: Subtitulo que especifica la funcion del modulo

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -5,6 +5,7 @@ const Orders = () => {
         <div>
             <h1 className='font-black text-4xl text-gray-500'>Lista de Pedidos</h1>
             <hr className='my-4' />
+            <h5 className="font-semibold text-lg text-gray-400">Este módulo permite al administrador gestionar pedidos</h5>
             <TablaOrders/>
         </div>
     );
