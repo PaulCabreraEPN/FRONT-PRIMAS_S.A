2a0e95316706969daf7cd91d0e51225cc874a0f6
OMEN <davidcatudiaz22@gmail.com>
2025-11-30 10:41:39 -0500

    Style: Cambio en el mensaje de la cabecera

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -3,9 +3,11 @@ import TablaOrders from "../components/TablaOrders";
 const Orders = () => {
     return(
         <div>
-            <h1 className='font-black text-4xl text-gray-500'>Lista de Pedidos</h1>
+            <h1 className='font-black text-4xl text-gray-500'>Lista de Ordenes</h1>
             <hr className='my-4' />
-            <h5 className="font-semibold text-lg text-gray-400">Este módulo permite al administrador gestionar pedidos</h5>
+            <h5 className="font-semibold text-lg text-gray-400">Este módulo permite al administrador visualizar las órdenes</h5>
+            <hr className='my-4' />
+
             <TablaOrders/>
         </div>
     );
