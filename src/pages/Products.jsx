249fd8b982cdb3a8da5031a0d28aa87b70ebcac5
OMEN <davidcatudiaz22@gmail.com>
2025-11-30 10:41:49 -0500

    Style: Cambio en el mensaje de la cabecera

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -8,7 +8,8 @@ const Products = () => {
     return (
         <div>
             <h1 className='font-black text-4xl text-gray-500'>Lista de Productos</h1>
-
+            <hr className='my-4' />
+            <h5 className="font-semibold text-lg text-gray-400">Este módulo permite al administrador visualizar los productos</h5>
             <hr className='my-4' />
             
             <TablaProducts/>
